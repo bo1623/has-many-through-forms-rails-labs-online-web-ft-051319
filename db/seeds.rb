4c322fc646ab7eeeecc4aba8dbfa31ee44d7c649
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-07-27 12:56:58 +0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+funny=Category.create(name:"funny")
+sad=Category.create(name:"sad")
+
+post1=Post.create(title: "first", content:"Testing")
+
+user1=User.create(username: 'CoolGuy5')
+user2=User.create(username: 'CoolGuy7')
